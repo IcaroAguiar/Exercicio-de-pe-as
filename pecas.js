aba8a6d3074c63500c21db8374e3bfef132591ef
IcaroAguiar <icaroaguiar14@gmail.com>
2022-06-13 17:39:58 -0300

    Entrega do trabalho de peças

---
+++ pecas.js
@@ -0,0 +1,27 @@
+let pesoPeca = 500  
+let numeroPecas = 7
+let nomePeca = "Rodas"
+
+if (pesoPeca > 100){
+    console.log("Pode cadastrar a peça")
+
+} else{
+    console.log("Impossível cadastrar")
+
+} 
+
+if(numeroPecas >= 10){
+    console.log("Impossível cadastrar, número de peças excedente")
+
+} else{
+    console.log("Pode seguir com o cadastro")
+
+}
+
+if (nomePeca.length < 3){
+    console.log("Nome da peça inválido")
+
+}else{
+    console.log("Nome da peça válido")
+
+}
